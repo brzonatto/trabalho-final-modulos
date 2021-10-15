53e0f0901ff613ae3e43b7d3db8cb20793699856
Brunno Zonatto <brzonatto@gmail.com>
2021-10-15 20:13:59 -0300

    ADD classe PokemonLendario

---
+++ ProjetoPokemon/src/com/dbc/PokemonLendario.java
@@ -0,0 +1,22 @@
+package com.dbc;
+
+import java.util.ArrayList;
+
+public class PokemonLendario extends Pokemon {
+    private String regiaoDominante;
+
+    public PokemonLendario(String nome, Integer numero, Double altura, Double peso, String categoria,
+                           Integer sexo, ArrayList<Tipo> tipos, Integer level, ArrayList<Habilidade> habilidades,
+                           Status status, Evolucao evolucao, String regiaoDominante) {
+        super(nome, numero, altura, peso, categoria, sexo, tipos, level, habilidades, status, evolucao);
+        this.regiaoDominante = regiaoDominante;
+    }
+
+    public String getRegiaoDominante() {
+        return regiaoDominante;
+    }
+
+    public void setRegiaoDominante(String regiaoDominante) {
+        this.regiaoDominante = regiaoDominante;
+    }
+}
