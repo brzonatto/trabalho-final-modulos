8f7b08488c04ccd34c45b0cad99dea4b29f855da
Brunno Zonatto <brzonatto@gmail.com>
2021-10-15 20:01:32 -0300

    ADD classe Status

---
+++ ProjetoPokemon/src/com/dbc/Status.java
@@ -0,0 +1,71 @@
+package com.dbc;
+
+public class Status {
+    private Integer hp;
+    private Integer ataque;
+    private Integer defesa;
+    private Integer especialAtaque;
+    private Integer especialDefesa;
+    private Integer velocidade;
+
+    public Status() {
+
+    }
+
+    public Status(Integer hp, Integer ataque, Integer defesa, Integer especialAtaque, Integer especialDefesa, Integer velocidade) {
+        this.hp = hp;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.especialAtaque = especialAtaque;
+        this.especialDefesa = especialDefesa;
+        this.velocidade = velocidade;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public Integer getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(Integer ataque) {
+        this.ataque = ataque;
+    }
+
+    public Integer getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(Integer defesa) {
+        this.defesa = defesa;
+    }
+
+    public Integer getEspecialAtaque() {
+        return especialAtaque;
+    }
+
+    public void setEspecialAtaque(Integer especialAtaque) {
+        this.especialAtaque = especialAtaque;
+    }
+
+    public Integer getEspecialDefesa() {
+        return especialDefesa;
+    }
+
+    public void setEspecialDefesa(Integer especialDefesa) {
+        this.especialDefesa = especialDefesa;
+    }
+
+    public Integer getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(Integer velocidade) {
+        this.velocidade = velocidade;
+    }
+}
