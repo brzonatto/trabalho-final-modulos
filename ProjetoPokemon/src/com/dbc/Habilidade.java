36da1fde9f5d034050e9056466872c0d33bbcd92
Brunno Zonatto <brzonatto@gmail.com>
2021-10-15 20:08:37 -0300

    ADD classe Habilidade

---
+++ ProjetoPokemon/src/com/dbc/Habilidade.java
@@ -0,0 +1,21 @@
+package com.dbc;
+
+public class Habilidade {
+    private String nome;
+
+    public Habilidade() {
+
+    }
+
+    public Habilidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
