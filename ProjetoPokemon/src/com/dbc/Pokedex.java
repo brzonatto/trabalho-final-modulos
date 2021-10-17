093ef145de570ef14adb88952768c086a01171aa
Leo-kardo <leonardo_c_g@hotmail.com>
2021-10-17 00:31:57 -0300

    Buscar Pokemon na pokedex adicionado

--- ProjetoPokemon/src/com/dbc/Pokedex.java
+++ ProjetoPokemon/src/com/dbc/Pokedex.java
@@ -1,96 +1,40 @@
 package com.dbc;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class Pokedex {
-    ArrayList<Pokemon> listaPokemon = new ArrayList<Pokemon>();
-    ArrayList<Tipo> listaTipos = new ArrayList<Tipo>();
-    ArrayList<Habilidade> listaHabilidades = new ArrayList<Habilidade>();
+    ArrayList<Pokemon> grupoPokemon;
 
-    public Pokedex(ArrayList<Pokemon> listaPokemon, ArrayList<Tipo> listaTipos, ArrayList<Habilidade> listaHabilidades) {
-        this.listaPokemon = listaPokemon;
-        this.listaTipos = listaTipos;
-        this.listaHabilidades = listaHabilidades;
+
+    public Pokedex(ArrayList<Pokemon> grupoPokemon) {
+        this.grupoPokemon= grupoPokemon;
     }
 
-    public ArrayList<Pokemon> getListaPokemon() {
-        return listaPokemon;
+    public void addPokemon(Pokemon pokemonCriado) {
+        grupoPokemon.add(pokemonCriado);
     }
 
-    public void setListaPokemon(ArrayList<Pokemon> listaPokemon) {
-        this.listaPokemon = listaPokemon;
+    public void removePokemon(Pokemon pokemonRetirado) {
+        grupoPokemon.remove(pokemonRetirado);
+
     }
 
-    public ArrayList<Tipo> getListaTipos() {
-        return listaTipos;
-    }
-
-    public void setListaTipos(ArrayList<Tipo> listaTipos) {
-        this.listaTipos = listaTipos;
-    }
-
-    public ArrayList<Habilidade> getListaHabilidades() {
-        return listaHabilidades;
-    }
-
-    public void setListaHabilidades(ArrayList<Habilidade> listaHabilidades) {
-        this.listaHabilidades = listaHabilidades;
-    }
-
-    public void adicionaPokemon(Pokemon novoPokemon) {
-        listaPokemon.add(novoPokemon);
-    }
-
-    public void retiraPokemon(Pokemon pokemonRetirado) {
-        Integer guardaIndice = -1;
-        for (int i = 0; i < listaPokemon.size(); i++) {
-            if (listaPokemon.get(i).equals(pokemonRetirado)) {
-                guardaIndice = i;
-            }
-        }
-        if (guardaIndice == -1) {
-            System.out.println("Pokémon não encontrado");
-        } else {
-            listaPokemon.remove(pokemonRetirado);
+    public void imprimeTodosPokemon(){
+        for (Pokemon pokemon : grupoPokemon) {
+            pokemon.imprimirPokemon();
         }
     }
 
-    public void adicionaTipo(Tipo novoTipo) {
-        listaTipos.add(novoTipo);
+    public ArrayList<Pokemon> getGrupoPokemon() {
+        return grupoPokemon;
     }
 
-    public void retiraTipo(Tipo tipoRetirado) {
-        Integer guardaIndice = -1;
-        for (int i = 0; i < listaPokemon.size(); i++) {
-            if (listaTipos.get(i).equals(tipoRetirado)) {
-                guardaIndice = i;
-            }
-        }
-        if (guardaIndice == -1) {
-            System.out.println("Tipo não encontrado");
-        } else {
-            listaTipos.remove(tipoRetirado);
-        }
-    }
-
-    public void adicionaHabilidade(Habilidade novaHabilidade) {
-        listaHabilidades.add(novaHabilidade);
-    }
-
-    public void retiraHabilidade(Habilidade habiliadeRetirada){
-        Integer guardaIndice = -1;
-        for (int i = 0; i < listaPokemon.size(); i++) {
-            if (listaHabilidades.get(i).equals(habiliadeRetirada)) {
-                guardaIndice = i;
-            }
-        }
-        if (guardaIndice == -1) {
-            System.out.println("Tipo não encontrado");
-        } else {
-            listaHabilidades.remove(habiliadeRetirada);
-        }
+    public void filtrarPorTipo(Tipo tipoPesquisado){
+        Stream<Pokemon> pokemonFiltrados = grupoPokemon.stream().filter(pokemon -> pokemon.getTipos().get(0).
+                equals(tipoPesquisado) || pokemon.getTipos().get(1).equals(tipoPesquisado));
+        pokemonFiltrados.forEach(System.out::println);
     }
 }
-
 
 
