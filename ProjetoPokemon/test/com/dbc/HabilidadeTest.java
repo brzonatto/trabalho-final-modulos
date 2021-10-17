b97a95e9336dc62d385109f4a595e555f1daa24b
Jonathas Magalhaes <jondejotam@gmail.com>
2021-10-17 10:16:04 -0300

    Create HabilidadeTest.java

---
+++ ProjetoPokemon/test/com/dbc/HabilidadeTest.java
@@ -0,0 +1,16 @@
+package com.dbc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HabilidadeTest {
+
+    @Test
+    void getMultiplicacaoDePoder() {
+    }
+
+    @Test
+    void setMultiplicacaoDePoder() {
+    }
+}
